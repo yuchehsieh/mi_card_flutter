28add10e384d580e636abf15f464d419e0d9cbdc
Murphy Hsieh <murphyhsieh@leandev.io>
2019-08-21 10:41:22 +0800

    [IntrinsicWidth / IntrinsicHeight]: make widgets inside Row or Column to be as tall/wide as the tallest/widest.

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,40 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    MyApp(),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Center(
+            child: IntrinsicWidth(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  RaisedButton(
+                    onPressed: () {},
+                    child: Text('Short'),
+                  ),
+                  RaisedButton(
+                    onPressed: () {},
+                    child: Text('A bit Longer'),
+                  ),
+                  RaisedButton(
+                    onPressed: () {},
+                    child: Text('The Longest text button'),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
