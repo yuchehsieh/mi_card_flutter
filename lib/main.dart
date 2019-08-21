b684e45b84f3e4f71e23e73c0aac7ec990363cef
Murphy Hsieh <murphyhsieh@leandev.io>
2019-08-21 11:07:13 +0800

    [constraints]: take the full/part of remaining space, apply in Container/ConstrainedBox

--- lib/main.dart
+++ lib/main.dart
@@ -9,29 +9,13 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    const iconSize = 50.0;
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: LayoutBuilder(
-            builder: (context, constraints) => Stack(
-              fit: StackFit.expand,
-              children: <Widget>[
-                Positioned(
-                  top: 0,
-//                  left: 0,
-                  child: Icon(Icons.star, size: iconSize),
-                ),
-                Positioned(
-                  top: constraints.maxHeight - iconSize,
-                  left: constraints.maxWidth - iconSize,
-//                  bottom: 0,
-//                  right: 0,
-                  child: Icon(Icons.call, size: iconSize),
-                ),
-              ],
-            ),
+          child: Container(
+            constraints: BoxConstraints.expand(height: 300),
+            color: Colors.amber,
           ),
         ),
       ),
