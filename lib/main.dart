e2bc1157033af97011a2de579bcf974394130b73
Murphy Hsieh <murphyhsieh@leandev.io>
2019-08-21 15:10:44 +0800

    add the two different font-family

--- lib/main.dart
+++ lib/main.dart
@@ -13,49 +13,34 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: LayoutBuilder(
-            builder: (context, constraints) {
-              return Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Container(
-                    constraints: BoxConstraints.expand(
-                      width: 100,
-                      height: constraints.maxHeight / 2,
-                    ),
-                    color: Colors.red,
-                  ),
-                  Container(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Container(
-                          width: 100,
-                          height: 100,
-                          color: Colors.yellow,
-                        ),
-                        Container(
-                          width: 100,
-                          height: 100,
-                          color: Colors.green,
-                        ),
-                      ],
-                    ),
-                  ),
-                  Container(
-                    constraints: BoxConstraints.expand(
-                      width: 100,
-                      height: constraints.maxHeight / 2,
-                    ),
-                    color: Colors.blue,
-                    margin: EdgeInsets.only(
-                      top: constraints.maxHeight / 2,
-                    ),
-                  )
-                ],
-              );
-            },
+          child: Column(
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage(
+                  'assets/images/murphy.jpeg',
+                ),
+              ),
+              Text(
+                'Muprhy Hsieh',
+                style: TextStyle(
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Pacifico',
+                ),
+              ),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.teal.shade100,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Source Sans Pro',
+                ),
+              )
+            ],
           ),
         ),
       ),
